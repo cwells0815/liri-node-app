d8f96c41d4deb1583ca49bb0c39594a880a51332
Nicholas Wells <n.wells3@icloud.com>
2018-07-26 21:12:20 -0400

    liri

--- liri.js
+++ liri.js
@@ -1,121 +1,141 @@
-require("env").config();
-var dataKeys = require("./keys.js");
-var fs = require('fs'); 
-var spotify = require('spotify');
-var request = require('request');
+require("dotenv").config();
 
 
-var writeToLog = function(data) {
-  fs.appendFile("log.txt", '\r\n\r\n');
 
-  fs.appendFile("log.txt", JSON.stringify(data), function(err) {
-    if (err) {
-      return console.log(err);
+// Packages and Modules
+var spotify = require("node-spotify-api");
+var request = require("request");
+var keys = require("./keys");
+var fs = require('fs');
+
+
+//Global variables
+var spotify = new spotify(keys.spotify);
+
+
+var nodeArgument = process.argv;
+var pickCase = process.argv[2];
+var inputText = "";
+
+for (var i = 3; i < nodeArgument.length; i++) {
+    if ((i > 3) && (i < nodeArgument.length)) {
+        inputText = inputText + "+" + nodeArgument[i];
+    } else {
+        inputText = inputText + nodeArgument[i];
     }
+}
+//This will be the switch statement that trigured certain functions upon our call
+switch (pickCase) {
+    case "spotify-this-song":
+        if (inputText) {
+            music(inputText);
+        } else {
+            music("Headstone")
+        };
 
-    console.log("log.txt was updated!");
-  });
+        break;
+
+    case "movie-this":
+        if (inputText) {
+            omdb(inputText);
+        } else {
+            omdb('Mr. Nobody')
+        };
+
+        break;
+
+    case "do-what-it-says":
+        doIt();
+
+        break;
+};
+
+function music(song) {
+    spotify.search({
+        type: "track",
+        query: song
+    }, function (error, data) {
+        if(!error) {
+            for (var i = 0; i < data.tracks.items.length; i++){
+                var songData = data.tracks.items[i];
+                console.log("---------------------------------");
+                console.log("Track Name: " + songData.name);
+                console.log("Artist: " + songData.artists[0].name);
+                console.log("Album: " + songData.album.name);
+                console.log("URL: " + songData.preview_url);
+                console.log("---------------------------------");
+
+                //This will be apart of the bonus
+                log("---------------------------------");
+                log("Track Name: " + songdata.name);
+                log("Artist: " + songdata.artists[0].name);
+                log("Album: " + songdata.album.name);
+                log("URL: " + songdata.preview_url);
+                log("---------------------------------");
+            }
+        
+        }
+});
 }
 
-//spotify artist
-var getArtistNames = function(artist) {
-  return artist.name;
-};
+function omdb(movieName) {
+    // Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-//spotify song
-var getMeSpotify = function(songName) {
-  if (songName === undefined) {
-    songName = 'Gods Plan';
-  };
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
 
-  spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if (err) {
-      console.log('Error occurred: ' + err);
-      return;
-    }
+request(queryUrl, function(error, response, body) {
 
-    var songs = data.tracks.items;
-    var data = []; //empty array to hold data
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
 
-    for (var i = 0; i < songs.length; i++) {
-      data.push({
-        'artist(s)': songs[i].artists.map(getArtistNames),
-        'song name: ': songs[i].name,
-        'preview song: ': songs[i].preview_url,
-        'album: ': songs[i].album.name,
-      });
-    }
-    console.log(data);
-    writeToLog(data);
-  });
-};
+    // Parse the body of the site and recover just the imdbRating
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors:  " + JSON.parse(body).Actors);
 
-var getMeMovie = function(movieName) {
-
-  if (movieName === undefined) {
-    movieName = 'Mr Nobody';
+            //BONUS add movie to log.txt
+            log("Title: " + JSON.parse(body).Title);
+            log("Release Year: " + JSON.parse(body).Year);
+            log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            log("Country: " + JSON.parse(body).Country);
+            log("Language: " + JSON.parse(body).Language);
+            log("Plot: " + JSON.parse(body).Plot);
+            log("Actors: " + JSON.parse(body).Actors);  
+  } else {
+      console.log("ERROR!");
   }
+  if (movieName === "Mr. Nobody") {
+    console.log("-------------");
+    console.log("If you haven't watched 'Mr. Nobody', then you should: <http://www.imdb.com/title/tt0485947/>");
+    console.log("It's on Netflix!");
 
-  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json";
-
-  request(urlHit, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = [];
-      var jsonData = JSON.parse(body);
-
-      data.push({
-      'Title: ' : jsonData.Title,
-      'Year: ' : jsonData.Year,
-      'Rated: ' : jsonData.Rated,
-      'IMDB Rating: ' : jsonData.imdbRating,
-      'Country: ' : jsonData.Country,
-      'Language: ' : jsonData.Language,
-      'Plot: ' : jsonData.Plot,
-      'Actors: ' : jsonData.Actors,
-      'Rotten Tomatoes Rating: ' : jsonData.tomatoRating,
-      'Rotton Tomatoes URL: ' : jsonData.tomatoURL,
-  });
-      console.log(data);
-      writeToLog(data);
+    log("-------------");
+    log("If you haven't watched 'Mr. Nobody', then you should: <http://www.imdb.com/title/tt0485947/>");
+    log("It's on Netflix!");
 }
-  });
-
+});
 }
 
-var doWhatItSays = function() {
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    console.log(data);
-    writeToLog(data);
-    var dataArr = data.split(',')
-
-    if (dataArr.length == 2) {
-      pick(dataArr[0], dataArr[1]);
-    } else if (dataArr.length == 1) {
-      pick(dataArr[0]);
-    }
-
-  });
+function doIt() {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        // DO NOT PUT SPACES AROUND COMMA: random.txt has no spaces
+        var txt = data.split(',');
+        music(txt[1]);
+    });
 }
 
-var pick = function(caseData, functionData) {
-  switch (caseData) {
-    case 'spotify-this-song':
-      getMeSpotify(functionData);
-      break;
-    case 'movie-this':
-      getMeMovie(functionData);
-      break;
-    case 'do-what-it-says':
-      doWhatItSays();
-      break;
-    default:
-      console.log('LIRI doesn\'t know that');
-  }
+function log(logging){
+    fs.appendFile('log.txt', logging, function(error){
+        if(error){
+            throw error;
+        }
+    });
 }
-
-//run this on load of js file
-var runThis = function(argOne, argTwo) {
-  pick(argOne, argTwo);
-};
-
-runThis(process.argv[2], process.argv[3]);
